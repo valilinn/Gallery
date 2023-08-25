5f55a27f837e7c7b6959ac451ab10ec6ec47a853
valilinn <valentinalinchuk@gmail.com>
2023-08-25 22:55:29 +0200

    Initial commit

---
+++ Gallery/Gallery/Controllers/SplashViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Валентина Лінчук on 25/08/2023.
+//
+
+import UIKit
+import Lottie
+
+class SplashViewController: UIViewController {
+    @IBOutlet weak var animationView: LottieAnimationView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .repeat(2)
+        animationView!.animationSpeed = 0.5
+        animationView!.play()
+        view.addSubview(animationView!)
+    }
+
+
+}
+
